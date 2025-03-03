2dd368445f1e8d648edfa4d2191a6066ad108cf4
Almina Silnovic <alminasilnovic10@gmail.com>
2025-03-03 16:46:01 +0100

    feat: updated shell script

--- asciidocs/scripts/docker-convert.sh
+++ asciidocs/scripts/docker-convert.sh
@@ -1,23 +1,15 @@
 #!/usr/bin/env bash
 
 inputPath="$1"
-createSlides="$2"
 source $inputPath/scripts/docker-convert-util.sh
 ASCIIDOCTOR_VERSION="1.58"
 
 echo "input => $inputPath"
-echo "createSlides => $createSlides"
 echo building html
 
-if [ $createSlides = true ]; then
-    convertFilesToSlides "$inputPath" $ASCIIDOCTOR_VERSION
-fi
-
-#inputPath="$inputPath"
+convertFilesToSlides "$inputPath" $ASCIIDOCTOR_VERSION
 
 convertFilesToHTML "$inputPath" $ASCIIDOCTOR_VERSION
 
-# set permissions of output folder to the same as the input folder - fixes #1
-#if [ -d "$inputPath" ] && [ -d "$outputPath" ]; then
-#    chown $(stat "$inputPath" -c %u:%g) "$outputPath" -R
-#fi
+find $inputPath -depth -name "*.adoc" -print
+find $inputPath -depth -name "*.adoc" -delete
